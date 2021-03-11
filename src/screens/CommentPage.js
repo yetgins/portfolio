d8e67948b3e8b7000bd30e8503a871685e7b372f
Sait Ahmet <62621911+yetgins@users.noreply.github.com>
2021-03-11 20:27:34 +0300

    Add CommentPage

---
+++ src/screens/CommentPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import Comment from '../components/Comment/Comment'
+import NavBar from '../components/NavBar/NavBar'
+
+const CommentPage = () => {
+    return (
+        <div>
+            <NavBar />
+            
+        </div>
+    )
+}
+
+export default CommentPage
