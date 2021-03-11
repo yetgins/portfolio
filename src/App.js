33e7bf060088b4225403eba58a940c16e171c77d
Sait Ahmet <62621911+yetgins@users.noreply.github.com>
2021-03-11 20:26:01 +0300

    Add routing in App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from './screens/HomePage';
+import AboutPage from './screens/AboutPage';
+import ContactPage from './screens/ContactPage';
+import CommentPage from './screens/CommentPage';
+import MyWorksPage from './screens/MyWorksPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <div className='App'>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+        <Route path="/myworks">
+          <MyWorksPage />
+        </Route>
+        <Route path="/comments">
+          <CommentPage />
+        </Route>
+        <Route path="/contact">
+          <ContactPage />
+        </Route>
+      </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
